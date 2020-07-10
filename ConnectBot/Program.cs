53478b4f2b04a04ede26ed928d5ec05c37f9083b
joncloud <jdberube@gmail.com>
2020-07-09 20:27:47 -0700

    Changes target to .NET Core * Updates csproj to minimize code * Adds MonoGame NuGet reference * Updates mgcb to have global properties * Removes compiled content files

--- ConnectBot/Program.cs
+++ ConnectBot/Program.cs
@@ -2,7 +2,6 @@
 
 namespace ConnectBot
 {
-#if WINDOWS || LINUX
     /// <summary>
     /// The main class.
     /// </summary>
@@ -18,5 +17,4 @@ namespace ConnectBot
                 game.Run();
         }
     }
-#endif
 }
